62ff1bc66cb1acd4393e37dd37820ac6b7cf39f2
Frenxys <114827463+Frenxys@users.noreply.github.com>
2023-03-10 18:16:44 +0100

    update

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 20; i++) {
   let a = true,
       n;
   while(a) {
-    n = Math.floor(Math.random() * 50) + 1;
+    n = Math.floor(Math.random() * 49) + 1;
     a = numbers.includes(n);
   }
   numbers.push(n);
