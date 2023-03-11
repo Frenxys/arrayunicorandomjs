e842ed4b3261d8ed49d50069ba290540a9535021
Frenxys <eneazani2005@gmail.com>
2023-03-11 09:32:10 +0100

    yea

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-var numbers = []
-=======
 var numbers = [];
->>>>>>> d34bfc2798a14d0208a901e536b8a5e64a5c4122
 for (let i = 0; i < 20; i++) {
-  let a = true,n;
-  while(a) {
+  let n;
+  do{
     n = Math.floor(Math.random() * 49) + 1;
-    a = numbers.includes(n);
-  }
+  }while(numbers.includes(n))
   numbers.push(n);
 }
 numbers.forEach(number=>document.write(number+"<br>"));
